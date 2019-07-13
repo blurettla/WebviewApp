042df590d4c5e5efa80fb01c654d807bbd1deeaa
Reg <regginaldkembo@my.smccd.edu>
2019-07-13 03:40:54 +0200

    Included the Andela certificate, added a raw and XML folder with XML files and references in the Android Manifest as well as remove the onReceivedSslError method override which could introduce potential vulnerability to the app Added a View profile button on the Dashboard activity, added an image view and text views to the Profile page to display an image and the profile fields

--- app/src/main/java/com/kerno/webviewapp/WebviewActivity.java
+++ app/src/main/java/com/kerno/webviewapp/WebviewActivity.java
@@ -1,15 +1,21 @@
 package com.kerno.webviewapp;
 
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
+import android.net.http.SslError;
 import android.os.Bundle;
+import android.webkit.SslErrorHandler;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class WebviewActivity extends AppCompatActivity
     {
+        @SuppressLint("SetJavaScriptEnabled")
         @Override
         protected void onCreate(Bundle savedInstanceState)
             {
